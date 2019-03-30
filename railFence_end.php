9e93e6b862e455b7acf58c1cb249f35bbe795455
Daniel <daniel.jatkowski24@gmail.com>
2019-03-30 15:22:35 +0100

    Koniec

--- railFence_end.php
+++ railFence_end.php
@@ -6,39 +6,29 @@ if(!(isset($_POST['encode']) && isset($_POST['encode_rail']) || (isset($_POST['d
     function encode($text, $rails)
     {
         $codedMessage = codeMessage($text, $rails);
-
         $finalMessage = "";
-
         foreach($codedMessage as $chunks) {
             $finalMessage .= $chunks;
         }
-
         return $finalMessage;
     }
-
     function decode($text, $rails)
     {
         $lengths = codeMessage($text, $rails);
-
         $reversedArray = array();
-
         $message = $text;
         for($i=0; $i<count($lengths); $i++) {
             $length = strlen($lengths[$i]);
-
             $reversedArray[] = substr($message, 0, $length);
             $message = substr($message, $length);
         }
-
         $decodedMessage = "";
-
         $k=0;
         $direction = "forwards";
         for($i=0;$i<strlen($text);$i++) {
             $currentLetter = $reversedArray[$k][0];
             $decodedMessage .= $currentLetter;
             $reversedArray[$k] = substr($reversedArray[$k], 1);
-
             if($k == 0) {
                 $k++;
                 $direction = "forwards";
@@ -51,27 +41,19 @@ if(!(isset($_POST['encode']) && isset($_POST['encode_rail']) || (isset($_POST['d
                 $k--;
             }
         }
-
         return $decodedMessage;
-
     }
-
     function codeMessage($text, $rails)
     {
         $codedMessage = array();
-
         $a=0;
         $direction = "forwards";
-
         for($i=0;$i<strlen($text);$i++) {
-
             $currentletter = $text[$i];
-
             if(!isset($codedMessage[$a])) {
                 $codedMessage[$a] = "";
             }
             $codedMessage[$a] .= $currentletter;
-
             if($a == 0) {
                 $a++;
                 $direction = "forwards";
@@ -83,9 +65,7 @@ if(!(isset($_POST['encode']) && isset($_POST['encode_rail']) || (isset($_POST['d
             }elseif($direction == "backwards") {
                 $a--;
             }
-
         }
-
         return $codedMessage;
     }
     if(!(isset($_POST['encode']) && isset($_POST['encode_rail']))){        
@@ -99,6 +79,6 @@ if(!(isset($_POST['encode']) && isset($_POST['encode_rail']) || (isset($_POST['d
         $zmienna1 = decode($_POST['decode'], $_POST['decode_rail']); 
         echo $zmienna1;
     }
-    echo '<br /><a href="index.php">Powrót</a>';
+    echo '<br /><a href="index.php" class="btn btn-primary">Powrót</a>';
 }
 ?>
