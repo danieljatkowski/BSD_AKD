93528b533b855a6d0180db5205c4b28c37dbee2f
crdiam <angiziewska@gmail.com>
2019-04-06 17:57:22 +0200

    Rows correction

--- Matrix.php
+++ Matrix.php
@@ -9,26 +9,47 @@ if(!(isset($_POST['encode_cipher']) && isset($_POST['encode_key']) || (isset($_P
     echo "Brak parametrów!";
 }else {
 
-    function encrypt($word, $key)
-    {
+    $lenKey = strlen($key);
+
+    function encrypt($word, $key) {
         $cipher = strtoupper(trim(""));
         $lenWord = strlen($word);
         $lenKey = strlen($key);
 
-        $rows = round($lenWord / $lenKey +  1);
+        if ($lenWord%$lenKey == 0) {
+            $rows = $lenWord/$lenKey;
+        }
+        else {
+            $rows = round($lenWord / $lenKey +  1);
+        }
+
 
         for ($i = 0; $i < $rows; $i++)
             for ($j = 0; $j < $lenKey; $j++) {
-                //$helperK = substr($key, $j, 1);
 
                 $nextI = $i * $lenKey + substr($key, $j, 1) -1;
-                $helperW = substr($word ,$nextI, 1);
                 if ($nextI < $lenWord) {
                     $cipher .= substr($word ,$nextI, 1);
                 }
             }
         return $cipher;
     }
+
+    //Moj poczatek, ale wrzucimy wersje Daniela ;p
+   /** function decode ($cipher, $key){
+            $word = strtoupper(trim(""));
+            $lenKey = strlen($key);
+            $reverseKey = $lenKey, ' ';
+            for ($i = 0; $i < $lenKey; $i++) {
+                $helper = substr($key, $i, 1) - 1;
+                $helperNew = substr($reverseKey, $helper, 1);
+                $helperNew = '1' + $i;
+            }
+            $rows = round(strlen($cipher) / $lenKey +  1);
+            for ($i =0; $i<$rows; $i++) {
+                $temp = strlen($key, ' ');
+            }
+    }*/
     if(!(isset($_POST['encode_key']) && isset($_POST['encode_cipher']))){
     }else{
         $zmienna = encrypt($_POST['encode_cipher'], $_POST['encode_key']);
