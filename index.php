426066a4989068be638ce2ee6ba805fee5a10bc5
Daniel <daniel.jatkowski24@gmail.com>
2019-04-07 02:03:27 +0200

    Koniec

--- index.php
+++ index.php
@@ -61,10 +61,17 @@
             <div class="col-lg-3">
                 <h1>Macierze</h1><br />
                 <form method="POST" action="transposition.php">
-                    <b>Do zakodowania/odkodowania: </b>
+                    <b>Do zakodowania: </b>
                     <input type="text" class="form-control" name="encode"><br />
                     <b>Szyfr: </b>
                     <input type="text" class="form-control" name="encode_cipher"><br />
+                    <input type="submit" class="btn btn-primary" value="Wyślij">
+                </form>
+                <form method="POST" action="transposition.php">
+                    <b>Do odkodowania: </b>
+                    <input type="text" class="form-control" name="decode"><br />
+                    <b>Szyfr: </b>
+                    <input type="text" class="form-control" name="decode_cipher"><br />
                     <input type="submit" class="btn btn-primary" value="Wyślij">
                 </form>
             </div>
