8302c5de8d0a1fe1305a479ae4ec2047981c1ddb
Daniel <daniel.jatkowski24@gmail.com>
2019-04-13 10:02:03 +0200

    Koniec

--- Matrix.php
+++ Matrix.php
@@ -5,11 +5,9 @@
  * Date: 03.04.2019
  * Time: 20:05
  */
-if(!(isset($_POST['encode_cipher']) && isset($_POST['encode_key']) || (isset($_POST['decode_cipher']) && isset($_POST['decode_key'])))){
+if(!(isset($_POST['encode']) && isset($_POST['encode_cipher']) || (isset($_POST['decode']) && isset($_POST['decode_cipher'])))){
     echo "Brak parametrów!";
 }else {
-
-    $lenKey = strlen($key);
 
     function encrypt($word, $key) {
         $cipher = strtoupper(trim(""));
@@ -35,30 +33,70 @@ if(!(isset($_POST['encode_cipher']) && isset($_POST['encode_key']) || (isset($_P
         return $cipher;
     }
 
-    //Moj poczatek, ale wrzucimy wersje Daniela ;p
-   /** function decode ($cipher, $key){
-            $word = strtoupper(trim(""));
-            $lenKey = strlen($key);
-            $reverseKey = $lenKey, ' ';
-            for ($i = 0; $i < $lenKey; $i++) {
-                $helper = substr($key, $i, 1) - 1;
-                $helperNew = substr($reverseKey, $helper, 1);
-                $helperNew = '1' + $i;
+    function decrypt($tekst, $szyfr){
+
+        $szyfr = str_replace("-", "", $szyfr);
+        $szyfrArr = array();
+        for( $i = 0; $i < strlen($szyfr) ; $i++ ){
+            $szyfrArr[$i] = $szyfr[$i];  
+        }
+        $tekst = strtoupper($tekst); 
+        $TAH = ceil(strlen($tekst)/strlen($szyfr));
+        
+        $tekstArr = array();
+        $tekstArri = array();
+        $k = 0;
+        for( $i = 0 ; $i < $TAH ; $i++ ){
+            for( $j = 0 ; $j < strlen($szyfr) ; $j++){
+                if(!isset($tekst[$k])){
+                    $tekstArr[$i][$j] = "/";
+                    $k++;
+                }else{
+                    $tekstArr[$i][$j] = $tekst[$k];
+                    $k++;
+                }
             }
-            $rows = round(strlen($cipher) / $lenKey +  1);
-            for ($i =0; $i<$rows; $i++) {
-                $temp = strlen($key, ' ');
+        }
+        $m = 0;
+        for( $i = 0 ; $i < $TAH ; $i++ ){
+            for( $j = 0 ; $j < strlen($szyfr) ; $j++){
+                if(!isset($tekstArr[$i][$j])){
+                     
+                }else{
+                    $tekstArri[$i][$szyfrArr[$m]] = $tekstArr[$i][$j];
+                }
+                if(0 <= $m && $m < strlen($szyfr)-1){
+                    $m++;
+                }else{
+                    $m = 0;
+                    break;
+                }
             }
-    }*/
-    if(!(isset($_POST['encode_key']) && isset($_POST['encode_cipher']))){
+        }
+        $odkodowany = "";
+        for( $i = 0 ; $i < $TAH ; $i++ ){
+            for( $j = 0 ; $j < count($szyfrArr) ; $j++){
+                if(isset($tekstArri[$i][$j])){
+                    if($tekstArri[$i][$j] == "/"){
+                        $odkodowany .= "";
+                    }else{
+                        $odkodowany .= $tekstArri[$i][$j];
+                    }
+                    
+                }
+            }
+        }
+        echo $odkodowany;
+    }
+    if(!(isset($_POST['encode']) && isset($_POST['encode_cipher']))){
     }else{
-        $zmienna = encrypt($_POST['encode_cipher'], $_POST['encode_key']);
+        $zmienna = encrypt($_POST['encode'], $_POST['encode_cipher']);
         echo $zmienna;
     }
 
-    if(!(isset($_POST['decode_key']) && isset($_POST['decode_cipher']))){
+    if(!(isset($_POST['decode']) && isset($_POST['decode_cipher']))){
     }else{
-        $zmienna1 = decrypt($_POST['decode_cipher'], $_POST['decode_key']);
+        $zmienna1 = decrypt($_POST['decode'], $_POST['decode_cipher']);
         echo $zmienna1;
     }
     echo '<br /><a href="index.php" class="btn btn-primary">Powrót</a>';
