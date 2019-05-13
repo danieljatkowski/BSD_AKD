ebec49b00f51d1db7edb599e799afcd588721919
crdiam <angiziewska@gmail.com>
2019-05-13 22:05:03 +0200

    Do 10 + deklaracja tablic na 11 i 12

--- DES.php
+++ DES.php
@@ -44,6 +44,48 @@ if($_FILES['txt']['error'] != 0){
             $C = array();
             $D = array();
             $CiDbyPC2 = array();
+            $KRXor =array();
+            $s1 = array(
+                0=> array(14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7) ,
+                1=> array( 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8) ,
+                2=> array(4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0) ,
+                3=>array(15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 ),);
+	        $s2 = array(
+                0=>array(15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10) ,
+                1=>array(3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5) ,
+                2=>array(0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15) ,
+                3=>array(13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 ),);
+	        $s3 = array(
+                0=>array(10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8) ,
+                1=>array(13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1) ,
+                2=>array(13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7) ,
+                3=>array(1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 ),);
+	        $s4= array(
+                0=>array(7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15) ,
+                1=>array(13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9) ,
+                2=>array(10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4) ,
+                3=>array(3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 ),);
+	        $s5 = array(
+                0=>array(2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9) ,
+                1=>array(14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6) ,
+                2=>array(4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14) ,
+                3=>array(11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 ),);
+	        $s6= array(
+                0=>array(12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11) ,
+                1=>array(10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8) ,
+                2=>array(9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6) ,
+                3=>array(4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 ),);
+	        $s7 = array(
+	            0=>array(4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1) ,
+                1=>array(13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6) ,
+                2=>array(1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2) ,
+                3=>array(6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 ),);
+	        $s8= array(
+                0=>array(13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7) ,
+                1=>array(1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2) ,
+                2=>array(7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8) ,
+                3=>array(2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 ),);
+
             //1.
             echo "<b>Tekst jawny: </b>" . $tekst . "<br />";
             echo "<b>Jego odpowiednik binarny:</b> " . $bin . "<br/>";
@@ -170,8 +212,59 @@ if($_FILES['txt']['error'] != 0){
                 echo "<br>";
                 $z++;
             }
-        }
 
+            //9.
+            for ($i=0; $i<count($CiDbyPC2); $i++) {
+                $KRXor[$i]= (int)$CiDbyPC2[$i]^(int)$right[$i];
+            }
+            echo "<b>KeyKRXor: </b>";
+            foreach ($KRXor as $keyKRXor) {
+                echo $keyKRXor;
+            }
+
+            //10.
+
+            $string1 = array_slice($KRXor, 0, 6);
+            $string2 = array_slice($KRXor, 6, 6);
+            $string3 = array_slice($KRXor, 12, 6);
+            $string4 = array_slice($KRXor, 18, 6);
+            $string5 = array_slice($KRXor, 24, 6);
+            $string6 = array_slice($KRXor, 30, 6);
+            $string7 = array_slice($KRXor, 36, 6);
+            $string8 = array_slice($KRXor, 42, 6);
+            echo "<b><br/>String 1: </b>";
+            foreach ($string1 as $keystr1) {
+                echo $keystr1;
+            }
+            echo "<b><br/>String 2: </b>";
+            foreach ($string2 as $keystr2) {
+                echo $keystr2;
+            }
+            echo "<b><br/>String 3: </b>";
+            foreach ($string3 as $keystr3) {
+                echo $keystr3;
+            }
+            echo "<b><br/>String 4: </b>";
+            foreach ($string4 as $keystr4) {
+                echo $keystr4;
+            }
+            echo "<b><br/>String 5: </b>";
+            foreach ($string5 as $keystr5) {
+                echo $keystr5;
+            }
+            echo "<b><br/>String 6: </b>";
+            foreach ($string6 as $keystr6) {
+                echo $keystr6;
+            }
+            echo "<b><br/>String 7: </b>";
+            foreach ($string7 as $keystr7) {
+                echo $keystr7;
+            }
+            echo "<b><br/>String 8: </b>";
+            foreach ($string8 as $keystr8) {
+                echo $keystr8;
+            }
+        }
 
         if (!(isset($_POST['tekst']))) {
         } else {
